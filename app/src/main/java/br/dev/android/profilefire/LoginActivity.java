1979c2639b117526dc2a2b8977d6d12f24d2783a
Kleitomberg <51670381+Kleitomberg@users.noreply.github.com>
2023-02-12 15:50:38 -0300

    criando tela CadastroActivity

--- app/src/main/java/br/dev/android/profilefire/LoginActivity.java
+++ app/src/main/java/br/dev/android/profilefire/LoginActivity.java
@@ -2,13 +2,29 @@ package br.dev.android.profilefire;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private TextView telaCadastro;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        this.telaCadastro = findViewById(R.id.new_conta);
+        this.telaCadastro.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        if(view.getId() == R.id.new_conta){
+            Intent intent = new Intent(this, CadastroActivity.class);
+            startActivity(intent);
+        }
+
     }
 }
