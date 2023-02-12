e4c7c78a7cab576abf89d6b40fb9e106efe4b758
Kleitomberg <51670381+Kleitomberg@users.noreply.github.com>
2023-02-12 16:00:23 -0300

    navegando entre as telas Login <-> cadastro

--- app/src/main/java/br/dev/android/profilefire/CadastroActivity.java
+++ app/src/main/java/br/dev/android/profilefire/CadastroActivity.java
@@ -2,13 +2,28 @@ package br.dev.android.profilefire;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class CadastroActivity extends AppCompatActivity {
+public class CadastroActivity extends AppCompatActivity  {
+
+    private TextView toLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+        this.toLogin = findViewById(R.id.new_login);
+
+        this.toLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CadastroActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
